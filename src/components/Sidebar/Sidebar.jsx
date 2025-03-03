508166b6e8568fe817bb20bf55e4ebcde4e94e83
shreyshreygowda <156389514+shreyshreygowda@users.noreply.github.com>
2025-03-03 14:32:59 -0800

    Updated font + menu opening

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
     return (
         <div className='sidebar'>
             <div className='top'>
-                <img className='menu' src={assets.menu_icon} alt="" />
+                <img onClick={()=>setExtended(prev=>!prev)} className='menu' src={assets.menu_icon} alt="" />
                 <div className="new-chat">
                     <img src={assets.plus_icon} alt="" />
                     {extended ? <p>New Chat</p> : null}
@@ -24,20 +24,20 @@ const Sidebar = () => {
                     </div>
                     : null
                 }
-                
+
             </div>
             <div className='bottom'>
                 <div className="bottom-item recent-entry">
                     <img src={assets.question_icon} alt="" />
-                    <p>Help</p>
+                    {extended?<p>Help</p>:null}
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.history_icon} alt="" />
-                    <p>Activity</p>
+                    {extended?<p>Activity</p>:null}
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.setting_icon} alt="" />
-                    <p>Settings</p>
+                    {extended?<p>Settings</p>:null}
                 </div>
             </div>
         </div>
