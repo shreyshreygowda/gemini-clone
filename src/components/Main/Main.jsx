9928d4d9a640f9775731ecfd0c4654473436e109
shreyshreygowda <156389514+shreyshreygowda@users.noreply.github.com>
2025-03-04 10:39:52 -0800

    result output - needs debugging

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -14,6 +14,9 @@ const Main = () => {
             <img src={assets.user_icon} alt="" />
         </div>  
         <div className="main-container">
+
+            {!showResult
+            ?<>
             <div className="greet">
                 <p><span>Hello, user.</span></p>
                 <p>How can I help you today?</p>
@@ -36,6 +39,18 @@ const Main = () => {
                     <img src={assets.code_icon} alt=""/>
                 </div>
             </div>
+            </>
+            :<div className='result'>
+                <div className="result-title">
+                    <img src={assets.user_icon} alt="" />
+                    <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                    <img src={assets.gemini_icon} alt="" />
+                    <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                </div>
+            </div>
+            }
 
             <div className="main-bottom">
                 <div className="search-box">
@@ -43,7 +58,7 @@ const Main = () => {
                     <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
+                        <img onClick={()=>onSent(input)} src={assets.send_icon} alt="" />
                     </div>
                 </div>
                 <p className="bottom-info">
