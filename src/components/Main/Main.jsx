cfc37751b94a26c9c4ec2ae756bc6aab179669c3
shreyshreygowda <156389514+shreyshreygowda@users.noreply.github.com>
2025-03-03 21:33:29 -0800

    Updated fonts + added 4 recommended search options

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,13 +11,25 @@ const Main = () => {
         </div>  
         <div className="main-container">
             <div className="greet">
-                <p><span>Hello, Shreya.</span></p>
+                <p><span>Hello, user</span></p>
                 <p>How can I help you today?</p>
             </div>
             <div className="cards">
                 <div className="card">
                     <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.bulb_icon} alt=""/>
+                </div>
+                <div className="card">
+                    <p>Briefly summarize this concept: urban planning</p>
                     <img src={assets.compass_icon} alt=""/>
+                </div>
+                <div className="card">
+                    <p>Brainstorm team bonding activities for our work retreat</p>
+                    <img src={assets.message_icon} alt=""/>
+                </div>
+                <div className="card">
+                    <p>Improve the readability of the following code</p>
+                    <img src={assets.code_icon} alt=""/>
                 </div>
             </div>
         </div>
