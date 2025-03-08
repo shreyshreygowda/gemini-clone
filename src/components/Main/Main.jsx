bc13074d7282197f5aad6949a67535b3c51fb12d
shreyshreygowda <156389514+shreyshreygowda@users.noreply.github.com>
2025-03-07 19:45:24 -0800

    Title UI Update

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
   return (
     <div className='main'>
         <div className="nav">
-            <p>Gemini</p>
+            <p>Gemini Clone</p>
             <img src={assets.user_icon} alt="" />
         </div>  
         <div className="main-container">
